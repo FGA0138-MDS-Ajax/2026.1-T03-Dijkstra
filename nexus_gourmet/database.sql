42d1a0d23df990674d2264c16e598ed7a646c573
El-flipante <alexandrev2909@gmail.com>
2026-07-01 12:19:27 -0300

    atualização banco de dados
    - id usuario, cpf usuario gora é a chave primaria da tabela users
    cpf agora é uma variavel do tipo texto
    conexões alteradas em outras tabelas para receberem cpf como foreing key
    criação do elemento 'data_criação -> datetime" na tabela 'tables'

--- nexus_gourmet/database.sql
+++ nexus_gourmet/database.sql
@@ -1,12 +1,6 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema nexus_db
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema nexus_db
@@ -20,15 +14,13 @@ USE `nexus_db` ;
 DROP TABLE IF EXISTS `nexus_db`.`users` ;
 
 CREATE TABLE IF NOT EXISTS `nexus_db`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `cpf` INT NOT NULL, 
+  `cpf` VARCHAR(11) NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `senha` VARCHAR(255) NOT NULL,
-  `cargo` ENUM('Administrador', 'Garçom', 'Cozinheiro') NOT NULL DEFAULT 'GARCOM',
+  `cargo` ENUM('ADMINISTRADOR', 'GARCOM', 'COZINHEIRO') NOT NULL DEFAULT 'GARCOM',
   `foto_usuario` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`cpf`))
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `nexus_db`.`tables`
@@ -36,12 +28,11 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `nexus_db`.`tables` ;
 
 CREATE TABLE IF NOT EXISTS `nexus_db`.`tables` (
-  `numero` INT NOT NULL AUTO_INCREMENT,
+  `numero` INT NOT NULL,
   `capacidade` INT NOT NULL,
-  `status` ENUM('Livre', 'Ocupada') NOT NULL DEFAULT 'Livre',
+  `status` ENUM('LIVRE', 'OCUPADA') NOT NULL DEFAULT 'LIVRE',
   PRIMARY KEY (`numero`))
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `nexus_db`.`products`
@@ -50,15 +41,13 @@ DROP TABLE IF EXISTS `nexus_db`.`products` ;
 
 CREATE TABLE IF NOT EXISTS `nexus_db`.`products` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `data_criacao` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `nome` VARCHAR(100) NOT NULL,
-  `preco` NUMERIC(10,2) NOT NULL,
-  `categoria` ENUM('Bebida', 'Prato', 'Sobremesa') NOT NULL,
+  `categoria` ENUM('BEBIDA', 'PRATO', 'SOBREMESA') NOT NULL,
+  `preco` DECIMAL(10,2) NOT NULL,
   `tempo_preparacao` INT NOT NULL DEFAULT 15,
   `foto_prato` VARCHAR(255) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `nexus_db`.`orders`
@@ -68,18 +57,18 @@ DROP TABLE IF EXISTS `nexus_db`.`orders` ;
 CREATE TABLE IF NOT EXISTS `nexus_db`.`orders` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `numero_diario` INT NOT NULL,
-  `data_abertura` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_criacao` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
   `entrada_cozinha` DATETIME NULL,
   `saida_cozinha` DATETIME NULL,
-  `status` ENUM('Finalizado', 'Em Preparo', 'Pronto', 'Entregue', 'Cancelado') NOT NULL DEFAULT 'EM_PREPARO',
+  `status` ENUM('FINALIZADO', 'ENTREGUE', 'PRONTO', 'EM_PREPARO', 'CANCELADO', 'PENDENTE') NOT NULL DEFAULT 'PENDENTE',
   `numero_mesa` INT NOT NULL,
-  `user_id` INT NOT NULL,
+  `user_cpf` VARCHAR(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_orders_users_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_orders_tables_idx` (`numero_mesa` ASC) VISIBLE,
+  INDEX `fk_orders_users_idx` (`user_cpf` ASC),
+  INDEX `fk_orders_tables_idx` (`numero_mesa` ASC),
   CONSTRAINT `fk_orders_users`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `nexus_db`.`users` (`id`)
+    FOREIGN KEY (`user_cpf`)
+    REFERENCES `nexus_db`.`users` (`cpf`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_orders_tables`
@@ -88,7 +77,6 @@ CREATE TABLE IF NOT EXISTS `nexus_db`.`orders` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `nexus_db`.`itens_ordered`
@@ -99,17 +87,17 @@ CREATE TABLE IF NOT EXISTS `nexus_db`.`itens_ordered` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `quantidade` INT NOT NULL DEFAULT 1,
   `observacao` VARCHAR(255) NULL,
-  `cozinha_status` VARCHAR(20) NOT NULL DEFAULT 'EM_PREPARO',
-  `preco_vendido` NUMERIC(10,2) NOT NULL DEFAULT 0.0,
+  `cozinha_status` VARCHAR(20) NOT NULL DEFAULT 'PENDENTE',
+  `preco_vendido` DECIMAL(10,2) NOT NULL DEFAULT 0.0,
   `order_id` INT NOT NULL,
   `product_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_itens_ordered_orders_idx` (`order_id` ASC) VISIBLE,
-  INDEX `fk_itens_ordered_products_idx` (`product_id` ASC) VISIBLE,
+  INDEX `fk_itens_ordered_orders_idx` (`order_id` ASC),
+  INDEX `fk_itens_ordered_products_idx` (`product_id` ASC),
   CONSTRAINT `fk_itens_ordered_orders`
     FOREIGN KEY (`order_id`)
     REFERENCES `nexus_db`.`orders` (`id`)
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_itens_ordered_products`
     FOREIGN KEY (`product_id`)
@@ -117,7 +105,6 @@ CREATE TABLE IF NOT EXISTS `nexus_db`.`itens_ordered` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
